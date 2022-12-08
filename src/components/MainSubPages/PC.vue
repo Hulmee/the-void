6a72d2a02c2c7605b080f537cee589cf29276e7e
Ellery Hulme <elleryh@insightsystems.com.au>
2022-12-08 11:34:09 +1100

    Changes to be committed: 	modified:   src/components/MainSubPages/PC.vue 	new file:   src/components/MainSubPages/SubpageSRL/IPTVbtn.vue

--- src/components/MainSubPages/PC.vue
+++ src/components/MainSubPages/PC.vue
@@ -1,15 +1,35 @@
 <template>
-  <section class=""></section>
+  <section class="container">
+    <h1 class="text">Plug your device into HDMI cable</h1>
+    <div>
+      <font-awesome-icon
+        class="icons"
+        icon="fa-solid fa-mobile-screen" />
+      <font-awesome-icon
+        class="icons"
+        icon="fa-solid fa-laptop" />
+      <font-awesome-icon
+        class="icons"
+        icon="fa-solid fa-computer" />
+    </div>
+  </section>
 </template>
 
 <script setup>
   //
 </script>
 
-<style>
+<style scoped>
   section {
     height: 90%;
     width: 90%;
     flex-direction: column;
+  }
+  .text {
+    font-size: 2em;
+  }
+  .icons {
+    padding: 1em;
+    height: 3em;
   }
 </style>
